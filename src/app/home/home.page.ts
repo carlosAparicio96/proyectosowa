be0224170565550a819aa08865a690b2f6e10c07
Javier <javier.maul@gmail.com>
2020-07-14 21:42:21 -0400

    Home
    cambios al home

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,6 +1,7 @@
 import { Component,OnInit } from '@angular/core';
 import { AppModule } from '../app.module';
 import { Router, ActivatedRoute, ParamMap, Routes, Params } from '@angular/router';
+import { MenuController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -8,6 +9,7 @@ import { Router, ActivatedRoute, ParamMap, Routes, Params } from '@angular/route
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  menu: any;
 
   constructor(private ar:ActivatedRoute, private router: Router) {}
 
@@ -26,6 +28,20 @@ export class HomePage {
   explorar(){
     var url=this.idUs+'/todas'
     this.router.navigate([url])
+  }
+
+  openFirst() {
+    this.menu.enable(true, 'first');
+    this.menu.open('first');
+  }
+
+  openEnd() {
+    this.menu.open('end');
+  }
+
+  openCustom() {
+    this.menu.enable(true, 'custom');
+    this.menu.open('custom');
   }
 
 
